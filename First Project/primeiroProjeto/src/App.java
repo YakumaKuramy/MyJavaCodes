1cb7ac2c5d87961d57c273c400c506745e07f67b
Yakuma Kuramy <104542682+YakumaKuramy@users.noreply.github.com>
2023-08-29 18:17:33 -0300

    Update
    Meu primeiro programa em Java.

---
+++ First Project/primeiroProjeto/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Ola, mundo!");
+        System.out.println("Meu primeiro codigo em Java")
+    }
+}
