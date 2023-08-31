8c2e263118fce7fd1514a77fb583b84ed0403d5f
Yakuma Kuramy <104542682+YakumaKuramy@users.noreply.github.com>
2023-08-31 16:07:23 -0300

    Update App.java
    Meu primeiro codigo usando a linguagem de programação Java

--- First Project/primeiroProjeto/src/App.java
+++ First Project/primeiroProjeto/src/App.java
@@ -1,7 +1,13 @@
+// O class significa classe
 public class App {
+    // a pavalvra main significa principal
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Ola, mundo!");
-        System.out.println("Meu primeiro codigo em Java")
+        System.out.println("Meu primeiro codigo em Java");
+        System.out.println("Aqui usei o 'sout' na criação desta linha ");
     }
 }
+// Logo eu tenho um metodo principal que esta na classe do primeiro programa
+// que jfaz parte de um pacote 
+
