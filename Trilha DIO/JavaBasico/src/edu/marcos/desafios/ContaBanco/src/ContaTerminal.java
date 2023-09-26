146a9d242ce98f971613702b032b1752b1a9bb92
Marcos Sousa <104542682+YakumaKuramy@users.noreply.github.com>
2023-09-26 01:03:58 -0300

    Deleting trash
    Exclusão de varios codigos obsoletos ou de teste. Criação de desafio ContaTerminal

---
+++ Trilha DIO/JavaBasico/src/edu/marcos/desafios/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, informe o numero da conta: ");
+        int numero = scanner.nextInt();
+        System.out.print("Por favor, informe o numero da agencia: ");
+        String agencia = scanner.next();
+        System.out.print("Por favor, informe seu nome: ");
+        String nomeCliente = scanner.next();
+        Double saldo = 120.40;
+
+        System.out.printf(
+            "Ola, %s, obrigado por criar uma conta em nosso banco, \nsua agencia é %s, conta %d, e seu saldo %.2f \njá esta disponivel para saque\n",
+            nomeCliente, agencia, numero, saldo
+            );
+
+    }
+}
